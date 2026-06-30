ef89a1653a9869d7210b4cd126423d3abf75abe3
AnishaKumari6 <anisha10021kumari@gmail.com>
2026-06-30 11:44:40 +0530

    day3

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,39 @@
-const button = document.getElementById("btn");
-const title = document.getElementById("title");
-const description = document.getElementById("description");
-const todoList = document.getElementById("todo-list");
+// const button = document.getElementById("btn");
+// const title = document.getElementById("title");
+// const description = document.getElementById("description");
+// const todoList = document.getElementById("todo-list");
+// button.addEventListener("click", function () {
+//     if (title.value.trim() === "" || description.value.trim() === "") {
+//         alert("Please fill all fields");
+//         return;}
+//     const li = document.createElement("div");
+//     li.classList.add("todo-item");
+//     const heading = document.createElement("h3");
+//     heading.innerText = title.value;
+//     const para = document.createElement("p");
+//     para.innerText = description.value;
+//     const deleteBtn = document.createElement("button");
+//     deleteBtn.innerText = "Delete";
+//     deleteBtn.addEventListener("click", function () {
+//         li.remove();
+//     });
+//     deleteBtn.style.backgroundColor="red";
+//     // deleteBtn.style.boarderRadius=45px
+//     li.appendChild(heading);
+//     li.appendChild(para);
+//     li.appendChild(deleteBtn);
+//     todoList.appendChild(li);
+//     title.value = "";
+//     description.value = "";
+// });
 
-button.addEventListener("click", function () {
-    if (title.value.trim() === "" || description.value.trim() === "") {
-        alert("Please fill all fields");
-        return;
-    }
-    const todo = document.createElement("div");
-    todo.classList.add("todo");
-    const heading = document.createElement("h3");
-    heading.innerText = title.value;
-    const para = document.createElement("p");
-    para.innerText = description.value;
-    const deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "Delete";
-    deleteBtn.addEventListener("click", function () {
-        todo.remove();
-    });
-    todo.appendChild(heading);
-    todo.appendChild(para);
-    todo.appendChild(deleteBtn);
-    todoList.appendChild(todo);
-
-    title.value = "";
-    description.value = "";
-});
+console.log('1');
+console.log('2');
+setTimeout(() => console.log('3'), 3000);
+setTimeout(() => console.log('4'), 1000);
+Promise.resolve().then(() => console.log('8'));
+console.log('5');
+setTimeout(() => console.log('6'), 2000);
+Promise.resolve().then(() => console.log('7'));
+console.log('9');
+// 1,2,5,9,8,7,4,6,3
